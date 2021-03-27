430ea24d750e4d914dcda0ff3ba0ae70d4d7af87
Danikelle Saraiva <danikellesaraiva@gmail.com>
2021-03-27 16:24:39 -0300

    modificado o arquivos.py

--- Arquivos/arquivos.py.py
+++ Arquivos/arquivos.py.py
@@ -192,3 +192,6 @@ print(dict3)
 # Exercício 10 - Considere a lista abaixo e retorne apenas os elementos cujo índice for maior que 5.
 lista = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
+for indice, valor in (enumerate(lista)):
+    if indice > 5:
+        print(valor)
